aebc8a4ae8c8fdc2ef37d9dca23457028ba0e877
varungolusupudi <varungolusupudi@gmail.com>
2025-05-17 15:35:28 -0700

    Added functionality to GET, SET, DEL and EXPIRE.

--- server/server.go
+++ server/server.go
@@ -5,25 +5,77 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"strconv"
 	"strings"
+	"sync"
+	"time"
 )
 
 var globalCache = make(map[string]interface{})
+var ttlMap = make(map[string]time.Time)
+var mu sync.RWMutex
 
-func handleGET() {
-
+func handleGET(args []string, conn net.Conn) {
+	if len(args) != 2 {
+		conn.Write([]byte("ERR usage: GET <key>\n"))
+		return
+	}
+	key := args[1]
+	if time.Now().After(ttlMap[key]) {
+		delete(ttlMap, key)
+		delete(globalCache, key)
+		conn.Write([]byte("Key expired\n"))
+		return
+	}
+	mu.RLock()
+	value, ok := globalCache[key]
+	mu.RUnlock()
+	if !ok {
+		conn.Write([]byte("nil\n"))
+		return
+	}
+	conn.Write([]byte(fmt.Sprintf("%v\n", value)))
 }
 
-func handleSET() {
-
+func handleSET(args []string, conn net.Conn) {
+	if len(args) != 3 {
+		conn.Write([]byte("ERR usage: SET <key> <value>\n"))
+		return
+	}
+	key := args[1]
+	value := args[2]
+	mu.Lock()
+	globalCache[key] = value
+	mu.Unlock()
+	conn.Write([]byte("OK\n"))
 }
 
-func handleDEL() {
-
+func handleDEL(args []string, conn net.Conn) {
+	if len(args) != 2 {
+		conn.Write([]byte("ERR usage: DEL <key>\n"))
+		return
+	}
+	key := args[1]
+	mu.Lock()
+	delete(globalCache, key)
+	mu.Unlock()
+	conn.Write([]byte("Deleted key\n"))
 }
 
-func handleEXPIRE() {
-
+func handleEXPIRE(args []string, conn net.Conn) {
+	if len(args) != 3 {
+		conn.Write([]byte("ERR usage: EXPIRE <key> <seconds>\n"))
+		return
+	}
+	key := args[1]
+	seconds, err := strconv.Atoi(args[2])
+	if err != nil {
+		conn.Write([]byte("ERR usage: EXPIRE <key> <seconds>\n"))
+		return
+	}
+	mu.Lock()
+	ttlMap[key] = time.Now().Add(time.Duration(seconds) * time.Second)
+	mu.Unlock()
 }
 
 func handleConnection(conn net.Conn) {
@@ -38,18 +90,17 @@ func handleConnection(conn net.Conn) {
 		}
 
 		operation := strings.Split(strings.ToUpper(message), " ")[0]
+		args := strings.Fields(message)
+
 		switch operation {
 		case "GET":
-			// Handle Get
-
-			handleGET()
+			handleGET(args, conn)
 		case "SET":
-
-			handleSET()
+			handleSET(args, conn)
 		case "DEL":
-			handleDEL()
+			handleDEL(args, conn)
 		case "EXPIRE":
-			handleEXPIRE()
+			handleEXPIRE(args, conn)
 		default:
 			log.Println("Unknown operation", operation)
 		}
